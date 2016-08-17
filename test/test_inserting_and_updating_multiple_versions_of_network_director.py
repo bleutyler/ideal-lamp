c3fd38a3eda14873a29089b9d7fb9011b7bef9f9
Tyler <tslijboom@juniper.net>
2016-08-17 21:19:30 +0000

    inc

--- test/test_inserting_and_updating_multiple_versions_of_network_director.py
+++ test/test_inserting_and_updating_multiple_versions_of_network_director.py
@@ -4,6 +4,7 @@ import sys
 import configparser
 import os
 import time
+import tempfile
 
 sys.path.append( '../bin' )
 
@@ -15,10 +16,10 @@ print( 'hey' )
 class UseCaseTester( unittest.TestCase ):
     applicationName = 'networkDirectoryAppTest'
     serverName      = 'testServer'
-    #app_homefolder  = '/home/bleutyler/git/tools--ini-config-manager/test/input_files'
-    app_homefolder  = '/home/tslijboom/git/tools--ini-config-manager/test/input_files'
+    #app_homefolder  = '/home/bleutyler/git/tools--ini-config-manager/test'
+    app_homefolder  = '/home/tslijboom/git/tools--ini-config-manager/test'
     user            = 'testClass'
-    test_configuration_file = '/home/tslijboom/git/tools--ini-config-manager/test/config_manager_tester.ini'
+    test_configuration_file = app_homefolder + '/config_manager_tester.ini'
 
     list_of_items_to_delete_at_the_end_of_testing = []
 
@@ -39,7 +40,7 @@ class UseCaseTester( unittest.TestCase ):
                 #self.assertTrue( 0 , msg= message )
 
             # Iterate through the config file of the version
-            sourceConfigFile = self.app_homefolder + '/config/network-directory.v.' + sourceVersion
+            sourceConfigFile = self.app_homefolder + '/input_files/config/network-directory.v.' + sourceVersion
             sourceConfig = configparser.ConfigParser()
             sourceConfig.read( sourceConfigFile )
             for section in sourceConfig.sections():
@@ -88,13 +89,34 @@ class UseCaseTester( unittest.TestCase ):
             table_definitions.currentConfigurationValues.changed_by_user == self.user ).all()
 
         if previousVersionsAlreadyAbsentTest:
-            message = 'found test version v' + sourceVersion + ' alraedy in the DB' 
+            message = 'found test version v' + testVersion + ' alraedy in the DB' 
             # why is this giving me an error?
-            #self.assertTrue( 0 , msg= message )
+            self.assertTrue( 0 , msg= message )
 
+        input_file = self.app_homefolder + '/input_files/config/network-directory.v.' + testVersion + '.ini'
+        temporaryOutputFile = tempfile.NamedTemporaryFile( delete=False )
+        template_file = self.app_homefolder + '/input_files/template/network-directory.v.' + testVersion + '.ini'
+        configFileHandle        = open( input_file, 'r' )
+        configFileBeforeTheTest = configFileHandle.read()
+        command_arguments_list = [ '-a', self.application, 
+                                   '-c', self.test_configuration_file, 
+                                   '-m', 'install',
+                                   '-s', self.serverName, 
+                                   #'-i', input_file, 
+                                   '-t', template_file, ]
+        with unittest.mock.patch( 'sys.argv', command_arguments_list ):
+            config_manager.main()
 
-        
+            # now check that the DB has the values
+            newVersionInsertedTest = table_definitions.session.query(
+                table_definitions.currentConfigurationValues ).filter( 
+                table_definitions.currentConfigurationValues.application_version == testVersion ).filter( 
+                table_definitions.currentConfigurationValues.changed_by_user == self.user ).all()
 
+            if not newVersionInsertedTest:
+                self.assertTrue( 0, 'failed to install a new application' )
+            else:
+                # nned to verify the values are correct
 
     # SETUP NEEDS IN INSTALL v2.0.0 for this
     def test_installation_of_application_adding_new_fields_previous_data ( self ):
